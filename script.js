dc2918c2cbfa2a37479a75e3820bfe13c12b529c
Mikhail <osano161@gmail.com>
2026-01-15 21:04:36 +0300

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const data = {
-  latte:{title:'Латте',text:'Состав: двойной эспрессо из 100% арабики, горячее молоко, воздушная молочная пена',img:'https://images.unsplash.com/photo-1541167760496-1628856ab772'},
-  cappuccino:{title:'Капучино',text:'Состав: эспрессо, горячее молоко и плотная молочная пена в равных пропорциях',img:'https://i-coffee.me/wp-content/uploads/2022/02/Coffee_Cappuccino_Cream_Cup_Saucer_525045_2048x1152.jpg'},
-  raf:{title:'Раф',text:'Состав: эспрессо, сливки 10%, ванильный сахар, взбитые до кремовой текстуры',img:'https://images.gastronom.ru/R2tO6aTuXyg6Hsqx6rwSMushYvJIv2VvDEzWfxibgI4/pr:recipe-cover-image/g:ce/rs:auto:0:0:0/L2Ntcy9hbGwtaW1hZ2VzL2JkYjE0OWFlLTIzNzMtNDk5Mi05NTY1LWYwNmJlOTExMjI1OC5qcGc.webp'},
-  americano:{title:'Американо',text:'Состав: эспрессо и горячая фильтрованная вода, сохраняющая аромат кофе',img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmjf5G4uhVhjlUhv2aqziCZ7558Bwxz8hRcQ&s'},
-  espresso:{title:'Эспрессо',text:'Состав: молотые зёрна 100% арабики, приготовленные под высоким давлением',img:'https://st8.stblizko.ru/images/product/291/294/024_big.png'},
-  flatwhite:{title:'Флэт уайт',text:'Состав: двойной эспрессо и небольшое количество горячего молока с микропеной',img:'https://shop.legend-tea.ru/upload/iblock/7fc/c0xmp2ox7tvkzay851zdppjxl8fa9703/ai-generated-cappuccino-cup-on-saucer-free-photo.jpeg'},
+  latte:{title:'Латте',text:'Состав: двойной эспрессо из 100% арабики, горячее молоко, воздушная молочная пена',img:'img/латте.jpg'},
+  cappuccino:{title:'Капучино',text:'Состав: эспрессо, горячее молоко и плотная молочная пена в равных пропорциях',img:'img/капучино.jpg'},
+  raf:{title:'Раф',text:'Состав: эспрессо, сливки 10%, ванильный сахар, взбитые до кремовой текстуры',img:'img/раф.png'},
+  americano:{title:'Американо',text:'Состав: эспрессо и горячая фильтрованная вода, сохраняющая аромат кофе',img:'img/американо.jpg'},
+  espresso:{title:'Эспрессо',text:'Состав: молотые зёрна 100% арабики, приготовленные под высоким давлением',img:'img/эспрессо.png'},
+  flatwhite:{title:'Флэт уайт',text:'Состав: двойной эспрессо и небольшое количество горячего молока с микропеной',img:'img/флетУайт.jpeg'},
 };
 
 const modal = document.getElementById('modal');
@@ -19,4 +19,5 @@ document.querySelectorAll('button[data-modal]').forEach(btn=>{
 });
 
 document.querySelector('.close').onclick=()=>modal.style.display='none';
+
 modal.onclick=e=>{ if(e.target===modal) modal.style.display='none'; }
